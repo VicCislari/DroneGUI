c0658775ca0f70c38999d132878621d4bf34ba99
VicCislari <victorion1402@gmail.com>
2024-01-11 15:14:28 +0100

    Adizens code

---
+++ src/ApiAdapter.java
@@ -0,0 +1,63 @@
+//package de.frauas.muellerbady.java.test;
+//author: Adizen
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.json.JSONTokener;
+
+public class ApiAdapter {
+
+    private static final String USER_AGENT = "Group 26";
+    private static final String URL = "https://dronesim.facets-labs.com/api/";
+    private static final String JSON_FORMAT = "/?format=json";
+    private static int offset;
+    private static final String QUERY = "&limit=10&offset=";
+    private static final String TOKEN = "Token 1bbbbd05efe3c733efcf8f443582a09cac4ca02c";
+    private static JSONObject jsonResponse;
+
+    public static JSONObject api_fetch(String category) {
+        URL url;
+        try {
+            url = new URL(URL + category + JSON_FORMAT + QUERY + offset);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestProperty("Authorization", TOKEN);
+            connection.setRequestMethod("GET");
+            connection.setRequestProperty("User-Agent", USER_AGENT);
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            JSONTokener tokener = new JSONTokener(in);
+            jsonResponse = new JSONObject(tokener);
+            in.close();
+
+        } catch (MalformedURLException e) {
+            System.err.println("Malformed URL: " + e.getLocalizedMessage());
+            e.printStackTrace();
+        }
+        catch (IOException e) {
+            System.out.println("General IO Exception: " + e.getLocalizedMessage());
+            e.printStackTrace();
+        }
+        return jsonResponse;
+    }
+    public static JSONArray api_results(String category){
+        JSONArray results = new JSONArray();
+        JSONObject fetch = api_fetch(category);
+        int j=0;
+        while(fetch.getInt("count") > j){ // while next block is not null
+            for (int i=0; i < fetch.getJSONArray("results").length(); i++) {
+                results.put(fetch.getJSONArray("results").getJSONObject(i));
+                j++;
+            }
+            offset = offset + 10;
+            fetch = api_fetch(category);
+        }
+        return results;
+    }
+}
---
+++ src/Drone.java
@@ -0,0 +1,51 @@
+//author: Adizen
+
+import java.time.LocalDateTime;
+public class Drone {
+    private int id;
+    private String drone_type;
+    private LocalDateTime created;
+    private String serial_number;
+    private int carriage_weight;
+    private String carriage_type;
+
+    // Getter
+    public int get_id(){
+        return id;
+    }
+    public String get_drone_type(){
+        return drone_type;
+    }
+    public LocalDateTime get_created(){
+        return created;
+    }
+    public String get_serial_number(){
+        return serial_number;
+    }
+    public int get_carriage_weight(){
+        return carriage_weight;
+    }
+    public String get_carriage_type(){
+        return carriage_type;
+    }
+
+    // Setter
+    public void set_id(int id){
+        this.id = id;
+    }
+    public void set_drone_type(String drone_type){
+        this.drone_type = drone_type;
+    }
+    public void set_created(String created){
+        this.created = LocalDateTime.parse(created);
+    }
+    public void set_serial_number(String serial_number){
+        this.serial_number = serial_number;
+    }
+    public void set_carriage_weight(int carriage_weight){
+        this.carriage_weight = carriage_weight;
+    }
+    public void set_carriage_type(String carriage_type){
+        this.carriage_type = carriage_type;
+    }
+}
