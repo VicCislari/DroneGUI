a1a320df23ca284815d32eba7b85f156170f6d28
WassabieX <warsay.kidane11@gmail.com>
2024-01-31 20:36:12 +0100

    small changes

---
+++ src/app/DroneGUIx.java
@@ -0,0 +1,32 @@
+package app;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+	/**
+	 * @functionality
+     * Simple FXML Loader for DroneGUI 
+     * 
+     * 
+	 * @author Wassabie // NEEDS FURTHER MODIFICATION FOR OTHER SPECS
+	 * @since 1.0
+	 * @last_modified 2024.01.31
+	 */
+
+public class DroneGUIx extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        Parent root = FXMLLoader.load(getClass().getResource("DroneGUI.fxml"));
+
+        primaryStage.setTitle("Dronefleet-Viewer");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
