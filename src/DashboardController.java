fc55c4fcaa1e5a8be5e95cf436dc6853b610feca
athee <atheesang@gmail.com>
2024-01-31 03:08:47 +0100

    kein fortschritt

--- src/DashboardController.java
+++ src/DashboardController.java
@@ -1,12 +1,21 @@
+import javafx.concurrent.Worker;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
-public class DashboardController {
+//50°39'35"N, 7°21'38"E → 49°32'34"N, 10°8'5"E
+public class DashboardController{
+    @FXML
+    private ImageView mapView;
+    @FXML
+    private BorderPane mainBody;
+
     @FXML
     private VBox vBoxButtonList;
-    @FXML
-    private Button droneButton;
+
     @FXML
     private void handleButtonAction() {
         // Logic for handling button click
@@ -15,11 +24,16 @@ public class DashboardController {
     public void initialize(){
         DroneTypeManager.initializeDroneTypes();
         DroneManager.initializeDrones();
+        Image mapImage = new Image("file:./map.png");
+        mapView.setImage(mapImage);
         for (int i = 0; i < DroneManager.getCount(); i++){
-            String droneButtonText = String.valueOf(DroneManager.getDroneList()[i].getId());
-            Button droneButton = new Button(droneButtonText);
+            int id = DroneManager.getDroneList()[i].getId();
+            String droneSerialNr = DroneManager.getDroneList()[i].getSerialNumber();
+            String droneButtonText = "ID:" + (id + " ");
+            Button droneButton = new Button(droneButtonText + droneSerialNr);
+            droneButton.setMaxWidth(Double.MAX_VALUE);
             vBoxButtonList.getChildren().add(droneButton);
         }
-
     }
+
 }
