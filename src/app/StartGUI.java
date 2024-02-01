ddb9fea5625ef18d9719c32cafe733765b8924be
WassabieX <warsay.kidane11@gmail.com>
2024-02-01 19:53:54 +0100

    Controller by Wassabie -- @AdiZen plz review (oder irgendjemand der weiß wie das geht -> Kommentar lesen)

--- src/app/StartGUI.java
+++ src/app/StartGUI.java
@@ -2,7 +2,10 @@ package app;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
 public class StartGUI {
 
@@ -13,8 +16,41 @@ public class StartGUI {
     private URL location;
 
     @FXML
-    void initialize() {
+    private Button continueButton;
+
+    @FXML
+    private Button exitButton;
+
+    private Stage stage;
+
+    public void setStage(Stage stage) {
+        this.stage = stage;
+    }
+
+    @FXML
+    void handleContinueClick(ActionEvent event) {
+        
+        // HIER NÄCHSTE GUI LADNE @AdiZen
 
     }
 
+    @FXML
+    void handleExitClick(ActionEvent event) {
+        if (stage != null) {
+            stage.close();
+        } else {
+            System.exit(0);
+        }
+    }
 }
+
+	/**
+	 * @functionality
+     * Controller for StartGUI.fxml
+     * 
+     * 
+	 * @author Wassabie
+	 * @since 1.0
+	 * @last_modified 2024.02.01
+     * 
+	 */
