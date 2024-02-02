513a6bdce52c8b0861d6b760420eaafe1d586630
WassabieX <warsay.kidane11@gmail.com>
2024-02-02 02:12:39 +0100

    small changes in FlightDynamics.java (Controller) still needs some adjustment, plz review guys

--- src/app/FlightDynamics.java
+++ src/app/FlightDynamics.java
@@ -3,6 +3,9 @@ package app;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class FlightDynamics {
 
@@ -13,8 +16,75 @@ public class FlightDynamics {
     private URL location;
 
     @FXML
-    void initialize() {
+    private TextField ID;
+
+    @FXML
+    private TextField battery;
+
+    @FXML
+    private Button buttonNext;
+
+    @FXML
+    private Button buttonPrevious;
+
+    @FXML
+    private TextField latitude;
+
+    @FXML
+    private TextField longitude;
+
+    @FXML
+    private TextField manufacturer;
+
+    @FXML
+    private TextField model;
+
+    @FXML
+    private Label page;
+
+    @FXML
+    private TextField pagejumper;
+
+    @FXML
+    private TextField pitch;
+
+    @FXML
+    private TextField roll;
+
+    @FXML
+    private TextField serialnumber;
+
+    @FXML
+    private TextField status;
+
+    @FXML
+    private Label totalPage;
+
+    @FXML
+    private TextField yaw;
+
+    @FXML
+    void handleNextClick(ActionEvent event) {
+
+        // NEXT PAGE
 
     }
+
+    @FXML
+    void handlePrevClick(ActionEvent event) {
+
+        // PREV PAGE
+        
+    }
+
+    @FXML
+    void jumptoPage(ActionEvent event) {
+
+        // JUMPING TO PAGE FUNCTION
+
+    }
+
+    // function for page & totalPage missing -> setter getter
+    // setter and getter methods for data missing
 
 }
